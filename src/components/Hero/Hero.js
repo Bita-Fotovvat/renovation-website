04b260a5c26b592397fdab1b0295da78dce65dc0
Bita Fotovvat <bita.ftvt@gmail.com>
2024-03-14 19:25:24 -0400

    added the quotation form on the hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -7,9 +7,16 @@ export default function Hero(){
     <section className="hero__parent">
         <section className="hero__child1">
             <h1 className="hero__brandname">Renontario</h1>
-            <p className="hero__about1">Building Dreams</p>
-            <p className="hero__about2">Renovating Realities</p>
-            <p className="hero__about3">Renovation <br/>& <br/>Remodeling</p>
+            <p className="hero__about1">slogan</p>
+            <p className="hero__about2">Renovation & Remodeling Company</p>
+            <p className="hero__about3">Need a Quote or Consultation?</p>
+            <p className="hero__about4">Enter your contact information and get a free quotation</p>
+            <form className="hero__form">
+                <input className="hero__form--name"  type="name" placeholder="Name" />
+                <input className="hero__form--email" type="email"  placeholder="Email" />
+                <input className="hero__form--phone" type="phone"  placeholder="Phone Number" />
+                <button>Get Quotation</button>
+            </form>
         </section>
         <section className="hero__child2">
             {/* <img src={HeroImage} alt="Hero Image" className="hero__image" /> */}
