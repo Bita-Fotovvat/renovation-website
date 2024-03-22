86d8e13b1d257fb624bb0cd95fea9e6852b35077
Bita Fotovvat <bita.ftvt@gmail.com>
2024-03-22 14:26:51 -0400

    fixing the form fields to get empty after submitting

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,20 +5,25 @@ import axios from "axios";
 import { useState, useEffect } from 'react';
 
 export default function Hero(){
+    const [formData, setFormData] = useState({
+        name: '',
+        city: '',
+        phone: '',
+        email: '',
+        message: ''
+    });
+    const handleInputChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({ ...formData, [name]: value });
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         try {
-            let formData = {
-                name: document.querySelector('.hero__form--name').value,
-                city: document.querySelector('.hero__form--city').value,
-                phone: document.querySelector('.hero__form--phone').value,
-                email: document.querySelector('.hero__form--email').value,
-                message: document.querySelector('.hero__form--message').value,
-            };
             const response = await axios.post(`http://localhost:8080/requests`, formData);
             if (response.status === 201) {
                 alert("Success! You will be contacted soon.");
+                setFormData({ name: '', city: '', phone: '', email: '', message: '' });
             } else {
                 console.error('Failed to submit:', response);
                 alert('Failed to submit your request. Please try again.');
@@ -44,12 +49,12 @@ export default function Hero(){
             {/* <p className="hero__about4">Enter your contact information to get a quotation</p> */}
             <form className="hero__form" onSubmit={handleSubmit}>
                 <section className="hero__form--parent">
-                    <input className="hero__form--name" name="name" type="name" placeholder="Name" />
-                    <input className="hero__form--city" name="city" type="city" placeholder="City" />
+                    <input className="hero__form--name" name="name" type="name" placeholder="Name"  value={formData.name} onChange={handleInputChange}/>
+                    <input className="hero__form--city" name="city" type="city" placeholder="City"  value={formData.city} onChange={handleInputChange} />
                 </section>
-                <input className="hero__form--phone" name="phone" type="tel" placeholder="Phone Number" />
-                <input className="hero__form--email" name="email" type="email"  placeholder="Email" />
-                <input className="hero__form--message" name="message" type="message"  placeholder="Message (optional)"/>
+                <input className="hero__form--phone" name="phone" type="tel" placeholder="Phone Number" value={formData.phone} onChange={handleInputChange}/>
+                <input className="hero__form--email" name="email" type="email"  placeholder="Email" value={formData.email} onChange={handleInputChange}/>
+                <input className="hero__form--message" name="message" type="message"  placeholder="Message (optional)" value={formData.message} onChange={handleInputChange}/>
                 {/* <input className="hero__form--message" type="phone"  placeholder="Message (optional)" /> */}
                 <button className="hero__form--button" type="submit">Submit</button>
             </form>
