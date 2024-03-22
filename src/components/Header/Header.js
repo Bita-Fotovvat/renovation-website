f3735e41cb85138ed0dfd30d6db5a1653816517a
Bita Fotovvat <bita.ftvt@gmail.com>
2024-03-22 14:08:32 -0400

    fixed posting the data from the form to the database

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,6 +12,7 @@ export default function Header(){
     <header className="header">
         <nav className="header__nav">
             <ul className="header__list">
+                <li className= "header__list--brandname" onClick={()=> navigate("/")}>Renontario</li>
                 <li className={`header__list--item ${activeLink === '/' ? 'headeractive':''}`} onClick={()=> {
                     navigate("/"); 
                     setActiveLink('/'); 
--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,9 +1,34 @@
 import "./Hero.scss";
 import HeroImage from "../../assets/images/Hero.png";
 import Logo from "../../assets/logo/logo.png";
+import axios from "axios";
 import { useState, useEffect } from 'react';
 
 export default function Hero(){
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            let formData = {
+                name: document.querySelector('.hero__form--name').value,
+                city: document.querySelector('.hero__form--city').value,
+                phone: document.querySelector('.hero__form--phone').value,
+                email: document.querySelector('.hero__form--email').value,
+                message: document.querySelector('.hero__form--message').value,
+            };
+            const response = await axios.post(`http://localhost:8080/requests`, formData);
+            if (response.status === 201) {
+                alert("Success! You will be contacted soon.");
+            } else {
+                console.error('Failed to submit:', response);
+                alert('Failed to submit your request. Please try again.');
+            }
+        } catch (error) {
+            console.error('Error saving to requests:', error);
+            alert('An error occurred while submitting your request. Please try again.');
+        }
+    };
+    
 
     return(
     <section className="hero__parent">
@@ -17,16 +42,16 @@ export default function Hero(){
             </section>
             {/* <p className="hero__about3">Schedule a Consultation</p> */}
             {/* <p className="hero__about4">Enter your contact information to get a quotation</p> */}
-            <form className="hero__form">
+            <form className="hero__form" onSubmit={handleSubmit}>
                 <section className="hero__form--parent">
-                    <input className="hero__form--name"  type="name" placeholder="Name" />
-                    <input className="hero__form--city" type="city"  placeholder="City" />
+                    <input className="hero__form--name" name="name" type="name" placeholder="Name" />
+                    <input className="hero__form--city" name="city" type="city" placeholder="City" />
                 </section>
-                <input className="hero__form--phone" type="phone"  placeholder="Phone Number" />
-                <input className="hero__form--email" type="email"  placeholder="Email" />
-                <textarea className="hero__form--message" name="message" rows="4" cols="50">Message (optional)</textarea>
+                <input className="hero__form--phone" name="phone" type="tel" placeholder="Phone Number" />
+                <input className="hero__form--email" name="email" type="email"  placeholder="Email" />
+                <input className="hero__form--message" name="message" type="message"  placeholder="Message (optional)"/>
                 {/* <input className="hero__form--message" type="phone"  placeholder="Message (optional)" /> */}
-                <button className="hero__form--button">Submit</button>
+                <button className="hero__form--button" type="submit">Submit</button>
             </form>
         </section>
         <section className="hero__child2">
