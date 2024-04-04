baa04aeade27a6288bcbc5d32b7f3983f82d355a
Bita Fotovvat <bita.ftvt@gmail.com>
2024-04-04 19:58:21 -0400

    projects page- phase 1 completed

---
+++ src/pages/MississaugaStoreProjPage/MississaugaStoreProjPage.js
@@ -0,0 +1,81 @@
+import "./MississaugaStoreProjPage.scss";
+import MississaugaStoreProj1 from "../../assets/images/5-mississauga-store-project/1.jpg";
+import MississaugaStoreProj2 from "../../assets/images/5-mississauga-store-project/2.jpg";
+import MississaugaStoreProj3 from "../../assets/images/5-mississauga-store-project/3.jpg";
+import MississaugaStoreProj4 from "../../assets/images/5-mississauga-store-project/4.jpg";
+import MississaugaStoreProj5 from "../../assets/images/5-mississauga-store-project/5.jpg";
+import MississaugaStoreProj6 from "../../assets/images/5-mississauga-store-project/6.jpg";
+import MississaugaStoreProj7 from "../../assets/images/5-mississauga-store-project/7.jpg";
+import MississaugaStoreProj8 from "../../assets/images/5-mississauga-store-project/8.jpg";
+import MississaugaStoreProj9 from "../../assets/images/5-mississauga-store-project/9.jpg";
+import MississaugaStoreProj10 from "../../assets/images/5-mississauga-store-project/10.jpg";
+import MississaugaStoreProj11 from "../../assets/images/5-mississauga-store-project/11.jpg";
+import MississaugaStoreProj12 from "../../assets/images/5-mississauga-store-project/12.jpg";
+import MississaugaStoreProj13 from "../../assets/images/5-mississauga-store-project/13.jpg";
+// import MississaugaStoreProj14 from "../../assets/images/5-mississauga-store-project/14.jpg";
+
+export default function MississaugaStoreProjPage(){
+    return(
+        <main>
+            <h1 className="mississaugastoreproj__maintitle">Burlington Project</h1>
+            <section className="mississaugastoreproj__imageparent">
+                <section className="mississaugastoreproj__imageparent1">
+                    <img className="mississaugastoreproj__image--1" src={MississaugaStoreProj1} alt="living room renovation drywall and framing" />
+                    <img className="mississaugastoreproj__image--2" src={MississaugaStoreProj2} alt="dining room renovation drywall and framing" />
+                </section>
+                <section className="mississaugastoreproj__imageparent2">
+                    <img className="mississaugastoreproj__image--3" src={MississaugaStoreProj3} alt="bedroom renovation drywall and framing" />
+                    <img className="mississaugastoreproj__image--4" src={MississaugaStoreProj4} alt="bedroom renovation drywall" />
+                </section>
+                <section className="mississaugastoreproj__imageparent3">
+                    <img className="mississaugastoreproj__image--5" src={MississaugaStoreProj5} alt="renovation ceiling drywall" />
+                    <img className="mississaugastoreproj__image--6" src={MississaugaStoreProj6} alt="renovation plumbing & electrical services" />
+                </section>
+                <section className="mississaugastoreproj__imageparent4">
+                    <img className="mississaugastoreproj__image--7" src={MississaugaStoreProj7} alt="bedroom renovation" />
+                    <img className="mississaugastoreproj__image--8" src={MississaugaStoreProj8} alt="living room renovation" />
+                </section>
+                <section className="mississaugastoreproj__imageparent5">
+                    <img className="mississaugastoreproj__image--9" src={MississaugaStoreProj9} alt="bathroom renovation" />
+                    <img className="mississaugastoreproj__image--10" src={MississaugaStoreProj10} alt="kitchen renovation" />
+                </section>
+                <section className="mississaugastoreproj__imageparent6">
+                    <img className="mississaugastoreproj__image--11" src={MississaugaStoreProj11} alt="bathroom renovation" />
+                    <img className="mississaugastoreproj__image--12" src={MississaugaStoreProj12} alt="kitchen renovation" />
+                </section>
+                <section className="mississaugastoreproj__imageparent7">
+                    <img className="mississaugastoreproj__image--13" src={MississaugaStoreProj13} alt="bathroom renovation" />
+                    {/* <img className="mississaugastoreproj__image--14" src={MississaugaStoreProj13} alt="bathroom renovation" /> */}
+                </section>
+            </section>
+
+
+            <section className="mississaugastoreproj__imageparentdesktop">
+                <section className="mississaugastoreproj__imageparentdesktop1">
+                    <img className="mississaugastoreproj__image--1" src={MississaugaStoreProj1} alt="living room renovation drywall and framing" />
+                    <img className="mississaugastoreproj__image--2" src={MississaugaStoreProj2} alt="dining room renovation drywall and framing" />
+                    <img className="mississaugastoreproj__image--3" src={MississaugaStoreProj3} alt="bedroom renovation drywall and framing" />
+                </section>
+                <section className="mississaugastoreproj__imageparentdesktop2">    
+                    <img className="mississaugastoreproj__image--4" src={MississaugaStoreProj4} alt="bedroom renovation drywall" />
+                    <img className="mississaugastoreproj__image--5" src={MississaugaStoreProj5} alt="renovation ceiling drywall" />
+                    <img className="mississaugastoreproj__image--6" src={MississaugaStoreProj6} alt="renovation plumbing & electrical services" />
+                </section>
+                <section className="mississaugastoreproj__imageparentdesktop3">
+                    <img className="mississaugastoreproj__image--7" src={MississaugaStoreProj7} alt="bedroom renovation" />
+                    <img className="mississaugastoreproj__image--8" src={MississaugaStoreProj8} alt="living room renovation" />
+                    <img className="mississaugastoreproj__image--9" src={MississaugaStoreProj9} alt="bathroom renovation" />
+                </section>
+                <section className="mississaugastoreproj__imageparentdesktop4">
+                    <img className="mississaugastoreproj__image--10" src={MississaugaStoreProj10} alt="kitchen renovation" />
+                    <img className="mississaugastoreproj__image--11" src={MississaugaStoreProj11} alt="bathroom renovation" />
+                    <img className="mississaugastoreproj__image--12" src={MississaugaStoreProj12} alt="kitchen renovation" />
+                </section>
+                <section className="mississaugastoreproj__imageparentdesktop5">
+                    <img className="mississaugastoreproj__image--13" src={MississaugaStoreProj13} alt="bathroom renovation" />
+                    {/* <img className="mississaugastoreproj__image--14" src={MississaugaStoreProj14} alt="bathroom renovation" /> */}
+                </section>
+            </section>
+        </main>
+    )
+}
