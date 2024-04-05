6d04005581d0ebaf7476bc709bd1565f61afed64
Bita Fotovvat <bita.ftvt@gmail.com>
2024-04-04 20:10:19 -0400

    our projects page completed

--- src/pages/OurProjectsPage/OurProjects.js
+++ src/pages/OurProjectsPage/OurProjects.js
@@ -12,26 +12,30 @@ export default function OurProjectsPage(){
         <main>
         <h1 className="projects__maintitle">Some of Our Projects</h1>
         <section className="projects__wrapper">
-            <section className="projects__niagaraone">
-                {/* <section className="projects__niagaraone--image" ></section> */}
-                <img className="projects__niagaraone--image" src={NaiagaraProject} alt="niagara renovation project" onClick={()=> {
+            <section className="projects__parent">
+                <section className="projects__niagaraone">
+                    {/* <section className="projects__niagaraone--image" ></section> */}
+                    <img className="projects__niagaraone--image" src={NaiagaraProject} alt="niagara renovation project" onClick={()=> {
                     navigate("/our-projects/niagara-1-project"); 
                     }}/>
-                <h2 className="projects__niagaraone--title" onClick={()=> {navigate("/our-projects/niagara-1-project");}}>Niagara Project</h2>
+                    <h2 className="projects__niagaraone--title" onClick={()=> {navigate("/our-projects/niagara-1-project");}}>Niagara Project</h2>
+                </section>
+                <section className="projects__oakvillebath">
+                    <img className="projects__oakvillebath--image" src={OakvilleBath} alt="oakville renovation project"  onClick={()=> {navigate("/our-projects/oakville-1-project");}}/>
+                    <h2 className="projects__oakvillebath--title" onClick={()=> {navigate("/our-projects/oakville-1-project");}}>Oakville Project</h2>
+                </section>
             </section>
-            <section className="projects__oakvillebath">
-                <img className="projects__oakvillebath--image" src={OakvilleBath} alt="oakville renovation project"  onClick={()=> {navigate("/our-projects/oakville-1-project");}}/>
-                <h2 className="projects__oakvillebath--title" onClick={()=> {navigate("/our-projects/oakville-1-project");}}>Oakville Project</h2>
-            </section>
-            <section className="projects__ancasterone">
-                <img className="projects__ancasterone--image" src={AncasterOneMain} alt="ancaster renovation project" onClick={()=> {
+            <section className="projects__parent">
+                <section className="projects__ancasterone">
+                    <img className="projects__ancasterone--image" src={AncasterOneMain} alt="ancaster renovation project" onClick={()=> {
                     navigate("/our-projects/ancaster-1-project"); 
                     }}/>
-                <h2 className="projects__ancasterone--title" onClick={()=> {navigate("/our-projects/ancaster-1-project");}}>Ancaster Project</h2>
-            </section>
-            <section className="projects__burligntonone">
-                <img className="projects__burligntonone--image" src={BurlingtonOneMain} alt="burlington renovation project"  onClick={()=> {navigate("/our-projects/burlington-1-project");}}/>
-                <h2 className="projects__burligntonone--title" onClick={()=> {navigate("/our-projects/burlington-1-project");}}>Bulington Project</h2>
+                    <h2 className="projects__ancasterone--title" onClick={()=> {navigate("/our-projects/ancaster-1-project");}}>Ancaster Project</h2>
+                </section>
+                <section className="projects__burligntonone">
+                    <img className="projects__burligntonone--image" src={BurlingtonOneMain} alt="burlington renovation project"  onClick={()=> {navigate("/our-projects/burlington-1-project");}}/>
+                    <h2 className="projects__burligntonone--title" onClick={()=> {navigate("/our-projects/burlington-1-project");}}>Bulington Project</h2>
+                </section>
             </section>
             <section className="projects__mississaugastore">
                 <img className="projects__mississaugastore--image" src={MississaugaStore} alt="mississauga store renovation"  onClick={()=> {navigate("/our-projects/mississauga-store-project");}}/>
