6b4f76c125a7345cfaab6de08a429ac28d758da4
Bita Fotovvat <bita.ftvt@gmail.com>
2024-05-06 15:30:25 -0400

    completed 1st version of the slideshow

--- src/components/SlideShow/SlideShow.js
+++ src/components/SlideShow/SlideShow.js
@@ -1,27 +1,64 @@
 import "./SlideShow.scss";
-import {useState} from 'react';
+import { useState, useEffect, useRef } from "react";
+import Image1 from "../../assets/images/slideshows/first-order.jpg";
+import Image2 from "../../assets/images/slideshows/deck.jpg";
+import Image3 from "../../assets/images/slideshows/aboutushero.jpg";
 
-export default function SlideShow(){
+export default function SlideShow() {
+    const [index, setIndex] = useState(0);
+    const timeoutRef = useRef(null);
+    const slideImages = [Image1, Image2, Image3];
+    const delay = 5000;
 
-    const Slideshow = ({ images }) => {
-        const [currentIndex, setCurrentIndex] = useState(0);
-    
-        const goToNext = () => {
-            const isLastSlide = currentIndex === images.length - 1;
-            const newIndex = isLastSlide ? 0 : currentIndex + 1;
-            setCurrentIndex(newIndex);
-        };
-    
-        return (
-            <div>
-                <img src={images[currentIndex]} alt={`Slide ${currentIndex}`} />
-                <button onClick={goToNext}>Next</button>
-            </div>
+    function resetTimeout() {
+        if (timeoutRef.current) {
+            clearTimeout(timeoutRef.current);
+        }
+    }
+
+    useEffect(() => {
+        resetTimeout();
+        timeoutRef.current = setTimeout(
+            () =>
+                setIndex((prevIndex) =>
+                    prevIndex === slideImages.length - 1 ? 0 : prevIndex + 1
+                ),
+            delay
         );
-    };
 
-    return(
-    <>
-    </>
+        return () => {
+            resetTimeout();
+        };
+    }, [index]);
+
+    return (
+        <div className="slideshow">
+            <div
+                className="slideshowSlider"
+                style={{ transform: `translate3d(${-index * 100}%, 0, 0)` }}
+            >
+                {slideImages.map((image, idx) => (
+                    <div
+                        className="slide"
+                        key={idx}
+                        style={{ backgroundImage: `url(${image})` }}
+                    ></div>
+                ))}
+            </div>
+
+            <div className="slideshowDots">
+                {slideImages.map((_, idx) => (
+                    <div
+                        key={idx}
+                        className={`slideshowDot${
+                            index === idx ? " active" : ""
+                        }`}
+                        onClick={() => {
+                            setIndex(idx);
+                        }}
+                    ></div>
+                ))}
+            </div>
+        </div>
     );
 }
